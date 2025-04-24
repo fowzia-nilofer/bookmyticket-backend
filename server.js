03c9815dd9a79771aeade3a48e17acae4162401a
Fowzia Nilofer <fowzia.nilofer@gmail.com>
2025-04-24 06:14:22 +0530

    Updated backend logic / routes / DB models (your change summary here)

--- server.js
+++ server.js
@@ -11,10 +11,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/bookmyticket', {
+// mongoose.connect('mongodb://127.0.0.1:27017/bookmyticket', {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true
+// })
+
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
@@ -22,5 +28,5 @@ app.use('/api/snacks', snackRoutes);
 app.use('/api/seats', seatRoutes);
 
 app.listen(5000, () => {
-    console.log("Server running on http://localhost:5000");
+    console.log("Server running on http://bookmyticket-backend-cg1z.onrender.com");
 });
